56feb08ce00c4344529ac93f910998acf173faa5
Dev Complete <163856724+devcomplete09@users.noreply.github.com>
2024-04-03 16:19:12 +0100

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return <div className="grid grid-flow-cols grid-cols-12">
+    <div className="col-span-12 mx-auto text-center mt-[5%] text-xl">
+      My app deployed on Render.com
+    </div>
+    <div className="col-span-12 text-center mt-4">
+      By Dev Complete
+    </div>
+  </div>
+}
